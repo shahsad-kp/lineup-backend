1a5d3366c47a2f77170312b0df60dd1261f2de95
Shahsad KP <shahsadkpklr@gmail.com>
2025-07-14 23:13:45 +0530

    Refactor serializers to streamline event and connection creation methods

--- MultiEventType/serializers.py
+++ MultiEventType/serializers.py
@@ -13,6 +13,16 @@ class MultiEventConnectionSerializer(ModelSerializer):
         model = MultiEventConnection
         exclude = ('multi_event_type',)
 
+    @classmethod
+    def create_connection(cls, data: dict) -> MultiEventConnection:
+        event_type_data = data['event_type']
+        event_type = EventTypeSerializer.create_event(event_type_data)
+        multi_event_connection = MultiEventConnection.objects.create(event_type=event_type, **data)
+        return multi_event_connection
+
+    def create(self, validated_data):
+        return self.create_connection(validated_data)
+
 
 class MultiEventTypeSerializer(ModelSerializer):
     event_types = MultiEventConnectionSerializer(many=True, source='event_type_connections')
@@ -30,3 +40,16 @@ class MultiEventTypeSerializer(ModelSerializer):
             'page_url',
             'event_types'
         ]
+
+    @classmethod
+    def create_multi_event_type(cls, data: dict) -> MultiEventType:
+        event_types_data = data.pop('event_types', [])
+        multi_event_type = MultiEventType.objects.create(**data)
+
+        for event_type_data in event_types_data:
+            MultiEventConnectionSerializer.create_connection(event_type_data | {'multi_event_type': multi_event_type})
+
+        return multi_event_type
+
+    def create(self, validated_data):
+        return self.create_multi_event_type(validated_data)
