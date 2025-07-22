e461e091572c71f4ae4dbbbe8708fb42b3d0e5b7
Shahsad KP <shahsadkpklr@gmail.com>
2025-07-22 22:36:47 +0530

    Add custom filter for status codes and enhance APIRequestLog admin interface

--- api_audit/admin.py
+++ api_audit/admin.py
@@ -1,12 +1,29 @@
-from django.contrib import admin
+from django.contrib.admin import EmptyFieldListFilter, SimpleListFilter, register, ModelAdmin
 
 from api_audit.models import APIRequestLog
 
 
-@admin.register(APIRequestLog)
-class APIRequestLogAdmin(admin.ModelAdmin):
+class UsedStatusCodeFilter(SimpleListFilter):
+    title = 'status code'
+    parameter_name = 'status_code'
+
+    def lookups(self, request, model_admin):
+        used_codes = list(set(APIRequestLog.objects.values_list('status_code', flat=True)))
+        return [
+            (code, dict(APIRequestLog._meta.get_field('status_code').choices).get(code, str(code)))
+            for code in sorted(used_codes)
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(status_code=self.value())
+        return queryset
+
+@register(APIRequestLog)
+class APIRequestLogAdmin(ModelAdmin):
     list_filter = [
         'method',
-        'status_code',
-        'user'
+        UsedStatusCodeFilter,
+        ('error_trace', EmptyFieldListFilter),
+
     ]
